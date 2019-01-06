102ca089ec6eff8b68004a0cc8e22ba3db90ed1e
yanuo <yanuo@wacai.com>
2019-01-06 23:49:51 +0800

    feat: update gitignore

---
+++ src/tool/start_service.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+PROJECTNAME=homepage
+
+source /etc/profile
+
+pid=`ps -ef |grep $PROJECTNAME |grep -v "grep" |awk '{print $2}'`
+
+if [ $pid ]; then
+
+​    echo "$PROJECTNAME  is  running  and pid=$pid"
+
+else
+
+   echo "Start success to start $PROJECTNAME ...."
+
+   nohup java -jar mini-program-0.0.1-SNAPSHOT.jar  >> catalina.out  2>&1 &
+
+fi
---
+++ src/tool/upload.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+scp -i ~/Documents/cert/mac_tencent_cloud /Users/zengwei/workspace/fushuspider/homepage/target/homepage-0.0.1-SNAPSHOT.jar ubuntu@111.230.196.36:/home/ubuntu/workspace/apetoday/
+scp -i ~/Documents/cert/mac_tencent_cloud /Users/zengwei/workspace/fushuspider/homepage/src/tool/start_service.sh ubuntu@111.230.196.36:/home/ubuntu/workspace/apetoday/
+scp -i ~/Documents/cert/mac_tencent_cloud /Users/zengwei/workspace/fushuspider/homepage/src/tool/stop_service.sh ubuntu@111.230.196.36:/home/ubuntu/workspace/apetoday/
+
+
