6a296ea288cd36088d23eac80b51ed5eb7dcc02b
yanuo <yanuo@wacai.com>
2019-01-07 14:44:43 +0800

    feat: add ssl cert files

--- src/tool/upload.sh
+++ src/tool/upload.sh
@@ -4,4 +4,7 @@ scp -i ~/Documents/cert/mac_tencent_cloud /Users/zengwei/workspace/fushuspider/h
 scp -i ~/Documents/cert/mac_tencent_cloud /Users/zengwei/workspace/fushuspider/homepage/src/tool/start_service.sh ubuntu@111.230.196.36:/home/ubuntu/workspace/apetoday/
 scp -i ~/Documents/cert/mac_tencent_cloud /Users/zengwei/workspace/fushuspider/homepage/src/tool/stop_service.sh ubuntu@111.230.196.36:/home/ubuntu/workspace/apetoday/
 
+scp -i ~/Documents/cert/mac_tencent_cloud /Users/zengwei/workspace/fushuspider/homepage/src/tool/1_www.apetoday.com_bundle.crt ubuntu@111.230.196.36:/home/ubuntu/workspace/apetoday/
+scp -i ~/Documents/cert/mac_tencent_cloud /Users/zengwei/workspace/fushuspider/homepage/src/tool/2_www.apetoday.com.key ubuntu@111.230.196.36:/home/ubuntu/workspace/apetoday/
+
 
