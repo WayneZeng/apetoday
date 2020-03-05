3da2f132c0575e5353eb041d41a14cc50a8cc6dd
yanuo <yanuo@wacai.com>
2020-03-05 16:16:39 +0800

    feat: 上传脚本修改

--- src/tool/upload.sh
+++ src/tool/upload.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-scp -i ~/Documents/cert/mac_tencent_cloud /Users/zengwei/workspace/fushuspider/homepage/target/homepage-0.0.1-SNAPSHOT.jar ubuntu@111.230.196.36:/home/ubuntu/workspace/apetoday/
-scp -i ~/Documents/cert/mac_tencent_cloud /Users/zengwei/workspace/fushuspider/homepage/src/tool/start_service.sh ubuntu@111.230.196.36:/home/ubuntu/workspace/apetoday/
-scp -i ~/Documents/cert/mac_tencent_cloud /Users/zengwei/workspace/fushuspider/homepage/src/tool/stop_service.sh ubuntu@111.230.196.36:/home/ubuntu/workspace/apetoday/
+scp -i ~/Documents/cert/mac_tencent_cloud /Users/zengwei/workspace/apetoday/homepage/target/homepage-0.0.1-SNAPSHOT.jar ubuntu@111.230.196.36:/home/ubuntu/workspace/apetoday/
+scp -i ~/Documents/cert/mac_tencent_cloud /Users/zengwei/workspace/apetoday/homepage/src/tool/start_service.sh ubuntu@111.230.196.36:/home/ubuntu/workspace/apetoday/
+scp -i ~/Documents/cert/mac_tencent_cloud /Users/zengwei/workspace/apetoday/homepage/src/tool/stop_service.sh ubuntu@111.230.196.36:/home/ubuntu/workspace/apetoday/
 
-scp -i ~/Documents/cert/mac_tencent_cloud /Users/zengwei/workspace/fushuspider/homepage/src/tool/1_www.apetoday.cn_bundle.crt ubuntu@111.230.196.36:/home/ubuntu/workspace/apetoday/
-scp -i ~/Documents/cert/mac_tencent_cloud /Users/zengwei/workspace/fushuspider/homepage/src/tool/2_www.apetoday.cn.key ubuntu@111.230.196.36:/home/ubuntu/workspace/apetoday/
+scp -i ~/Documents/cert/mac_tencent_cloud /Users/zengwei/workspace/apetoday/homepage/src/tool/1_www.apetoday.cn_bundle.crt ubuntu@111.230.196.36:/home/ubuntu/workspace/apetoday/
+scp -i ~/Documents/cert/mac_tencent_cloud /Users/zengwei/workspace/apetoday/homepage/src/tool/2_www.apetoday.cn.key ubuntu@111.230.196.36:/home/ubuntu/workspace/apetoday/
 
 
